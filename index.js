efc09809c5fe2f81a02bbe9d006f6dfedd81ea77
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-04-25 13:42:19 +0530

    programmaticaly

---
+++ index.js
@@ -0,0 +1,22 @@
+const lighthouse = require('lighthouse')
+const chrome = require('lighthouse/lighthouse-cli/chrome-launcher.js').ChromeLauncher
+const config = require('lighthouse/lighthouse-core/config/default.json')
+
+const flags = {}
+const url = 'https://example.com'
+
+const launcher = new chrome({ port: 9222, autoSelectChrome: true })
+
+launcher
+.isDebuggerReady()
+.catch(() => launcher.run()) // Launch Chrome
+.then(() => lighthouse(url, flags, config)) // Run Lighthouse
+.then(results => launcher.kill().then(() => results)) // Kill Chrome and return results
+.then(results => {
+  results.artifacts = undefined // Disable artifacts
+  console.log(results)
+})
+.catch(err => {
+  console.log(err)
+  launcher.kill().then(() => throw(err)) // Kill Chrome if there's an error.
+})
